28e36926f89e03a8c5f48f8dc7a03a4b107da99b
Grigoriy Romanov <grigoriy.romanov@unikie.com>
2022-10-19 12:30:13 +0300

    Change include path for asm-offsets.h
    For some undisclosed reason I cant create proper folder
    for headers in output directory with linux build artefacts.
    Something delete it after creation
    
    The commit is workaround for the issue
    
    Signed-off-by: Grigoriy Romanov <grigoriy.romanov@unikie.com>

--- core/hyp_config.h
+++ core/hyp_config.h
@@ -3,7 +3,7 @@
 #define __HYP_CONFIG_H__
 
 #ifndef STANDALONE
-#include "include/generated/asm-offsets.h"
+#include "includes/generated/asm-offsets.h"
 /*
  * Make sure the kernel build has been properly configured
  * for KVM extension.
