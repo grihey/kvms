8ca4058e65688eac3f4cd66cc0e9bb3836cbb6d5
Grigoriy Romanov <grigoriy.romanov@unikie.com>
2022-12-06 17:25:28 +0200

    Remove Q from linker recipe
    Signed-off-by: Grigoriy Romanov <grigoriy.romanov@unikie.com>

--- core/common.mk
+++ core/common.mk
@@ -53,7 +53,7 @@ $(PROG).bin: $(PROG)
 
 $(PROG): $(OBJS) $(OBJDIR)/$(LIBNAME) FORCE
 	$(vecho) [LD] $@
-	$(Q)$(LD) $(LDFLAGS) -lcore -o $(PROG) $(LDLIBS) $(GLIBC_PATH)/lib/static-stubs.o -static
+	$(LD) $(LDFLAGS) -lcore -o $(PROG) $(LDLIBS) $(GLIBC_PATH)/lib/static-stubs.o -static
 
 $(OBJDIR)/$(LIBNAME): $(OBJS)
 	$(vecho) [AR] $@
